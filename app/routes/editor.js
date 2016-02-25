bae4350facc0f192704b6671a6a246203ca716bb
maixezer <maixezer@gmail.com>
2016-02-25 14:42:44 +0700

    Add more style to draggable and droppable object, fix some bug of draggable in dropzone must not cloned.

--- app/routes/editor.js
+++ app/routes/editor.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import $ from 'jquery';
 
 export default Ember.Route.extend({
 
@@ -6,25 +7,75 @@ export default Ember.Route.extend({
     $('html').attr('class', 'layout-editor is-editor');
   },
 
+  model() {
+    let elements = JSON.parse([
+      '{',
+      '"width": 500,',
+      '"height": 500,',
+      '"elements": [',
+      '{',
+      '"id": "1",',
+      '"type": "Button",',
+      '"x": 10,',
+      '"y": 30,',
+      '"z": 2,',
+      '"width": 200,',
+      '"height": 300,',
+      '"text": "OK"',
+      '},',
+      '{',
+      '"id": "2",',
+      '"type": "RadioButton",',
+      '"x": 200,',
+      '"y": 200,',
+      '"z": 2,',
+      '"isChecked": true',
+      '},',
+      '{',
+      '"id": "3",',
+      '"type": "Panel",',
+      '"x": 5,',
+      '"y": 5,',
+      '"z": 1,',
+      '"width": 800,',
+      '"height": 800,',
+      '"childrenID": [1,2]',
+      '}]',
+      '}'].join(''));
+    return elements;
+  },
+
   actions: {
-    dropped(event, ui, _self) {
-      _self.$().droppable({
-        drop(event, ui) {
-          $(this).append($(ui.helper).clone().draggable());
-        }
+    didTransition() {
+      let serialized_json = this.modelFor('editor');
+      Ember.run.schedule('afterRender', this, () => {
+        $('.droppable-el').css({
+          'width': serialized_json.width,
+          'height': serialized_json.height
+        });
       });
     },
 
-    dragClone(event, ui, _self) {
+    dropped: Ember.on('didTransition', function (event, ui, _self) {
+      _self.$().droppable({
+        accept: '.draggable-el',
+        drop(event, ui) {
+          var newClone = $(ui.helper).clone()
+            .addClass('draggable-el-clone')
+            .removeClass('draggable-el')
+            .draggable({cursor: 'pointer'});
+          $(this).append(newClone);
+        }
+      });
+    }),
+
+    dragClone: Ember.on('didTransition', function (event, ui, _self) {
+      // revert animated
       _self.$().draggable({
         revert(event, ui) {
-          $(this).data("ui-draggable").originalPosition = {
-            top : 0,
-            left : 0
-          };
           return !event;
         }
       });
-    }
+    })
   }
 });
