65bbbf778aad62ed96d05adbc2241f7449c6db36
maixezer <maixezer@gmail.com>
2016-03-01 23:14:06 +0700

    Configure ember-simple-auth

--- app/authenticators/oauth2.js
+++ app/authenticators/oauth2.js
@@ -1,5 +1,6 @@
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
 
 export default OAuth2PasswordGrant.extend({
-  serverTokenEndpoint: 'http://api.mockit.co/oauth/token'
+  serverTokenEndpoint: 'http://localhost:3000/oauth/token',
+  serverTokenRevocationEndpoint: 'http://localhost:3000/oauth/revoke'
 });
