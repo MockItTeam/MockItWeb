9f5d56174a5b7713965031cbb7e47888b9a386e1
maixezer <maixezer@gmail.com>
2016-03-01 23:20:34 +0700

    Add signout Action.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin);
+const { service } = Ember.inject;
+
+export default Ember.Route.extend(ApplicationRouteMixin, {
+  actions: {
+    signout() {
+      // Fix broken layout when clearing the session
+      // The application template is based on the session state
+      this.disconnectOutlet('main');
+      this.get('session').invalidate();
+    }
+  }
+});
